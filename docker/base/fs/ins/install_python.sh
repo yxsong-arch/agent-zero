d16e6202fb1006b800be4302fdc651dcf26f1e80
linuztx <linuztx@gmail.com>
2025-09-11 12:12:51 +0800

    update torch versions

--- docker/base/fs/ins/install_python.sh
+++ docker/base/fs/ins/install_python.sh
@@ -47,8 +47,8 @@ source /opt/venv/bin/activate
 pip install --no-cache-dir --upgrade pip ipython requests
 # Install some packages in specific variants
 pip install --no-cache-dir \
-    torch==2.4.0 \
-    torchvision==0.19.0 \
+    torch==2.6.0 \
+    torchvision==0.21.0 \
     --index-url https://download.pytorch.org/whl/cpu
 
 echo "====================PYTHON UV ===================="
